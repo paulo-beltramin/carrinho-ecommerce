579aac9acc060c16ec1ba32c0940f82b40e472a5
paulo-beltramin <dev.beltramin@gmail.com>
2025-05-29 13:37:21 -0300

    finalizado responsividade para tablet, desktop e celulares

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,5 +1,6 @@
 import { useContext } from "react"
 import { cartContext } from "../../Components/Context/cartContext"
+import { Link } from "react-router-dom"
 
 
 const Cart = () => {
@@ -13,51 +14,66 @@ const Cart = () => {
         Meu carrinho
       </p>
 
-      {cart.map((item) => (
-        <section className="grid lg:flex md:grid-cols md:gap-y-4  justify-between items-center border-b-2 w-full" key={item.id}>
-          <div className="w-32">
-            <img src={item.cover} alt="imagem do produto" />
-          </div>
+      {cart.length <= 0 ? <>
+        <span className="text-center block text-2xl font-bold text-red-600">
+          Carrinho vazio
+        </span>
+        <Link to={'/'} className="flex justify-center">
+          <p className="bg-gray-800 text-white text-center px-4 py-1 w-52 mt-7
+          rounded ">
+            Ir para os produtos
+          </p>
+        </Link></> :
+        <>
+          {cart.length > 0 &&
+            <>
+              {cart.map((item) => (
+                <section key={item.id}>
+                  <div className="flex flex-col gap-4 lg:flex lg:flex-row lg:justify-between items-center border-b-2 w-full">
+                    <img className="w-32  " src={item.cover} alt="imagem do produto" />
+                    <strong> {item.price.toLocaleString("pt-br", {
+                      style: 'currency',
+                      currency: 'BRL'
+                    })}</strong>
 
-          <div>
-            <strong> {item.price.toLocaleString("pt-br", {
-              style: 'currency',
-              currency: 'BRL'
-            })}</strong>
-          </div>
 
-          <div className="flex gap-4 items-center">
-            <button onClick={() => removeItemCart(item)} className="bg-zinc-600 px-4 rounded font-extrabold text-white cursor-pointer">
-              -
-            </button>
+                    <div className="flex gap-4 items-center">
+                      <button onClick={() => removeItemCart(item)} className="bg-zinc-600 px-4 rounded font-extrabold text-white cursor-pointer">
+                        -
+                      </button>
 
-            <p>
-              {item.amount}
-            </p>
+                      <p>
+                        {item.amount}
+                      </p>
 
-            <button onClick={() => addItem(item)} className="bg-zinc-600 px-4 rounded font-extrabold text-white cursor-pointer">
-              +
-            </button>
-          </div>
+                      <button onClick={() => addItem(item)} className="bg-zinc-600 px-4 rounded font-extrabold text-white cursor-pointer">
+                        +
+                      </button>
+                    </div>
 
-          <div>
-            <strong>
-              Subtotal: {item.subTotal.toLocaleString("pt-br", {
-                style: 'currency',
-                currency: 'BRL'
-              })
+                    <div>
+                      <strong>
+                        Subtotal: {item.subTotal.toLocaleString("pt-br", {
+                          style: 'currency',
+                          currency: 'BRL'
+                        })
 
-              }
-            </strong>
-          </div>
-        </section>
-      ))}
+                        }
+                      </strong>
+                    </div>
+                  </div>
+                </section>
+              ))}
 
-      <div className="mt-4 font-medium">
-        <p>
-          Total: {total}
-        </p>
-      </div>
+              <div className="mb-12 mt-7 font-medium pl-4">
+                <p>
+                  Total: {total}
+                </p>
+              </div>
+            </>
+          }
+        </>
+      }
 
     </div>
   )
